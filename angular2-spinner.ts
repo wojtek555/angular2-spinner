a7809562007075c7aef0db82f1e30bc9f0e2ec53
Cosmin Albulescu <cosmin@albulescu.ro>
2016-09-09 13:33:34 +0300

    Fix

--- angular2-spinner.ts
+++ angular2-spinner.ts
@@ -4,7 +4,7 @@ import { Component, Input, OnInit } from '@angular/core';
   moduleId: module.id,
   selector: 'spinner',
   template: '',
-  style: [`
+  styles: [`
     @keyframes spin {
       from {transform: rotate(0deg);}
       to {transform: rotate(360deg);}
