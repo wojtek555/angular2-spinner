29a0d01f18a2c30163299ea829baf37ee82b5804
Cosmin Albulescu <cosmin@albulescu.ro>
2016-09-09 13:50:05 +0300

    Final

--- angular2-spinner.ts
+++ angular2-spinner.ts
@@ -4,26 +4,7 @@ import { Component, Input, OnInit } from '@angular/core';
   moduleId: module.id,
   selector: 'spinner',
   template: '',
-  styles: [`
-    @keyframes spin {
-      from {transform: rotate(0deg);}
-      to {transform: rotate(360deg);}
-    }
-
-    :host {
-      position:relative;
-      box-sizing: border-box;
-      display:inline-block;
-      padding:0px;
-      border-radius:100%;
-      border-style:solid;
-      animation: spin 0.8s linear infinite;
-    }
-
-    :host .margins {
-        margin: 0px 10px;
-    }
-  `],
+  styleUrls: ['angular2-spinner.css'],
   host: {
     '[style.width]':'size + "px"',
     '[style.height]':'size + "px"',
